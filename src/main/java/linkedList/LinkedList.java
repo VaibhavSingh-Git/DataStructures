067baafc09a1f8f168462a3aaef987df63df5635
vaibhav singh <aliassingh.0795@gmail.com>
2025-03-14 12:49:52 +0530

    sorted insertion in list

--- src/main/java/linkedList/LinkedList.java
+++ src/main/java/linkedList/LinkedList.java
@@ -1,36 +1,65 @@
 package linkedList;
 
 public class LinkedList<T> {
-    private static class Node<T>{
+
+    int size = 0;
+    private Node head = null;
+
+    @Override
+    public String toString() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        return "LinkedList " + "size= " + size + ", head=" + head ;
+    }
+
+    private static class Node<T> {
         T value;
         Node next;
+
         public Node(T value) {
             this.value = value;
             this.next = null;
         }
     }
 
-    int size = 0 ;
-    Node head = null;
-
-    public void insertAtHead(T value){
+    public void insertAtHead(T value) {
         Node current = new Node(value);
         current.next = head;
         head = current;
         size++;
     }
 
-    public void insertAtEnd(T value){
+    public void insertAtEnd(T value) {
         Node current = new Node(value);
-        if(head == null){
+        if (head == null) {
             head = current;
-        }else{
+        } else {
             Node temp = head;
-            while(temp.next != null){
+            while (temp.next != null) {
                 temp = temp.next;
             }
             temp.next = current;
-        }size++;
+        }
+        size++;
+    }
+
+    //sorted insertion
+    public void sortedInsert(T value){
+        Node current = new Node(value);
+        if(head == null || (Integer)head.value >= (Integer)current.value){
+            current.next = head;
+            head = current;
+        }else{
+            Node temp = head;
+            while(temp.next != null && (Integer)temp.next.value < (Integer)current.value){
+                temp = temp.next;
+            }current.next = temp.next;
+            temp.next = current;
+        }
+        size++;
     }
 
     public int getSize() {
