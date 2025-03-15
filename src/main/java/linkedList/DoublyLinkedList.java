ac8d9148506832f9fab8f245d471ba14fa442c00
vaibhav singh <143624994+VaibhavSingh-Git@users.noreply.github.com>
2025-03-15 21:55:15 +0530

    DoublyLinkedList

--- src/main/java/linkedList/DoublyLinkedList.java
+++ src/main/java/linkedList/DoublyLinkedList.java
@@ -1,7 +1,49 @@
 package linkedList;
 
 public class DoublyLinkedList {
-    public static void main(String[] args) {
 
+    Node head = null;
+    Node tail = null;
+    int size = 0;
+
+    //node class
+    private static class Node{
+        int value;
+        Node prev;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
     }
+
+    public void insertAtBeginning(int value){
+        Node temp = new Node(value);
+        if(head == null){
+            head = temp;
+            tail = temp;
+        }else{
+            head.prev = temp;
+            temp.next = head;
+            head = temp;
+        }size++;
+    }
+
+    public void insertAtEnd(int value){
+        Node temp = new Node(value);
+        if(head == null){
+            head = temp;
+        }else{
+            temp.prev = tail;
+        }
+        tail = temp;
+        size++;
+    }
+
+//    public void insertAtPosition(int value, int position){
+//        Node temp = new Node(value);
+//
+//    }
 }
