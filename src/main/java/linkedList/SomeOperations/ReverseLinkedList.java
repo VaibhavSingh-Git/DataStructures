f51046fc4e56b55030bbfd2c766901c319396422
vaibhav singh <143624994+VaibhavSingh-Git@users.noreply.github.com>
2025-03-19 23:52:01 +0530

    recursive reverse method

--- src/main/java/linkedList/SomeOperations/ReverseLinkedList.java
+++ src/main/java/linkedList/SomeOperations/ReverseLinkedList.java
@@ -3,7 +3,7 @@ package linkedList.SomeOperations;
 
 import linkedList.LinkedList;
 
-//This class contains method to reverse a given linkedList
+//This class contains methods to reverse a given linkedList
 public class ReverseLinkedList {
     //method to reverse a linked list
     public static void reverse(LinkedList<Integer> list){
@@ -29,6 +29,24 @@ public class ReverseLinkedList {
         list.toString();
     }
 
+    //recursive method for reversing a linked list
+    public static LinkedList.Node reverse(LinkedList.Node head){
+        if(head ==null){
+            return head;
+        }
+        LinkedList.Node p = null,q = null;
+        p = head;
+        q = p.next;
+        if(q == null){
+            return p;
+        }
+        q = reverse(q);
+        p.next.next = p;
+        p.next = null;
+        return q;
+    }
+
+
     public static void main(String[] args) {
         LinkedList<Integer> l = new LinkedList<>();
         l.sortedInsert(5);
@@ -36,7 +54,10 @@ public class ReverseLinkedList {
         l.sortedInsert(35);
         l.sortedInsert(25);
         l.sortedInsert(115);
+        //l.toString();
+        LinkedList.Node q = reverse(l.head);
+        l.head = q;
         l.toString();
-        reverse(l);
+        //reverse(l);
     }
 }
